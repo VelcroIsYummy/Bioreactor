667b5f42edab007ca1ab452940e6138f18b318e2
Anahad Khosa <Anahad.khosa@icloud.com>
2026-04-13 21:19:10 -0600

    Work

--- src/Main/Main.ino
+++ src/Main/Main.ino
@@ -1,4 +1,5 @@
 #include <Wire.h>
+#include <functional>
 
 /* Now I hate annotating my code, but since wilson will be working on this
 and the judges will be looking at it. I guess I can swallow my pride and just
@@ -21,32 +22,37 @@ class PIDControl {
     float setpoint; // Set points are our target temprature
     float val; // Our current value, eg. temp, pH, DO
     float error; // current error in temp
-    float preverror = 0 // previous error, required for derivative
+    float preverror = 0; // previous error, required for derivative
     float elapsedTime, Time, timePrev; // required for intergral
     std::function<float()> readSensor; // Bassically does a function
+    float PID_value;
 
-    int start (kp, ki, kd, setpoint, std::function<float()> sensorFn):
-    kp(kp), ki(ki), kd(kd), setpoint(setpoint), readSensor(sensorFn) {
+    int doPID (int kp, int ki, int kd, float setpoint,
+    std::function<float()> sensorFn)
+    {
+    this->kp = kp; this->ki = ki; this->kd = kd; this->setpoint = setpoint; 
+    this->readSensor = sensorFn;
       val = readSensor(); // Pass in function
      //Next we calculate the error between the setpoint and the real value
 
-      PID_error = set_temperature - temperature_read + 3;
+      error = setpoint - val + 3;
       //Calculate the P value
-      PID_p = 0.01*kp * PID_error;
+      PID_p = 0.01*kp * error;
       //Calculate the I value in a range on +-3
-      PID_i = 0.01*PID_i + (ki * PID_error);
+      PID_i = 0.01*PID_i + (ki * error);
       //For derivative we need real time to calculate speed change rate
       timePrev = Time;                            
       Time = millis();                            
       elapsedTime = (Time - timePrev) / 1000; 
       //Now we can calculate the D value
-      PID_d = 0.01*kd*((PID_error - previous_error)/elapsedTime);
+      PID_d = 0.01*kd*((error - preverror)/elapsedTime);
       //Final total PID value is the sum of P + I + D
       PID_value = PID_p + PID_i + PID_d;
       preverror = error;
 
-      constrain(PID_value, 0, 255); // stops errors cuz we can't have -pwm
-
+      return constrain(PID_value, 0, 255);
+      // stops errors because we can't have -pwm
+    }
   private:
     int PID_p = 0;    int PID_i = 0;    int PID_d = 0;
   
@@ -58,10 +64,17 @@ class PIDControl {
 
 void setup() {
   pinMode(PWM_pin,OUTPUT);
-  SetPinFrequencySafe(3, 928)
+  SetPinFrequencySafe(3, 928);
+  PIDControl tempLoop;
+}
+
+void loop() {
+  delay(50);
+  tempLoop.doPID(90,30,80, 39, checkThermocouple);
+  analogWrite(PWM_pin, tempLoop.PID_value);
 }
 
 
-
-
-analogWrite(PWM_pin,255-PID_value);
+float checkThermocouple() {
+  burger
+}
