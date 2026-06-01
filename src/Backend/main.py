4d58c43ec6b06030e8f340640e11c5352bf18136
Anahad Khosa <Anahad.khosa@icloud.com>
2026-06-01 17:12:06 -0600

    Added README, added a way to update the config.csv. Updated CAD.

--- src/Backend/main.py
+++ src/Backend/main.py
@@ -10,6 +10,27 @@ async def requestConfig():
   with open("config.csv") as config:
     return config.read();
 
+@api.post("/updateConfig")
+async def updateDb(request: Request):
+  data = await request.json();
+  sha256 = hashlib.sha256();
+  apiKey = "d34f3c6446b3c6250b5be1042f078049629c2c5ffb682bab73c89812f3240390";
+  if "key" in data:
+    sha256.update(data["key"].encode("UTF-8"));
+  else:
+    return 401;
+  recivedKey = sha256.hexdigest();
+  if recivedKey == apiKey:
+    try:
+      with open("config.csv", "w") as config:
+        dm1 = data["motor1MlPerMinute"];
+        dm2 = data["motor1MlPerMinute"];
+        dm3 = data["motor3MlPerMinute"];
+        config.write(f"{dm1}, {dm2}, {dm3}");
+        return 200;
+    except:
+      return 401;
+
 @api.get("/reciveTheEntireDatabase")
 async def requestDb():
   with open("database.csv") as database:
@@ -25,7 +46,6 @@ async def updateDb(request: Request):
   else:
     return 401;
   recivedKey = sha256.hexdigest();
-  print(recivedKey)
   if recivedKey == apiKey:
     try:
       with open("database.csv", "a") as database:
--- src/Bioreactor GUI/dosing.js
+++ src/Bioreactor GUI/dosing.js
@@ -7,6 +7,9 @@ const m3DoseSelector = document.getElementById("motor3MlPerMinute");
 const currMls1 = document.getElementById("ml1");
 const currMls2 = document.getElementById("ml2");
 const currMls3 = document.getElementById("ml3");
+let ML1 = 0;
+let ML2 = 0;
+let ML3 = 0;
 
 let options = {
   clean: true,
@@ -23,9 +26,12 @@ function updateDosingLabels() {
   console.log(databaseData);
   let datah = Papa.parse(databaseData);
   datah = datah["data"];
-  currMls1.innerHTML = datah[1];
-  currMls1.innerHTML = datah[2];
-  currMls1.innerHTML = datah[3];
+  ML1 = datah[1];
+  ML2 = datah[2];
+  ML3 = datah[3];
+  currMls1.innerHTML = ML1;
+  currMls2.innerHTML = ML2;
+  currMls3.innerHTML = ML3;
 }
 
 let client = mqtt.connect(broker, options);
@@ -33,16 +39,26 @@ client.on("connect", () => {
   console.log("amconnect");
 });
 
+function updateConfig() {
+  await fetch(databaseURL, {
+    method: "POST", body: `{"motor1MlPerMinute":"${ML1}", "motor2MlPerMinute":"${ML2}", "motor3MlPerMinute":"${ML3}"}`
+  });
+}
+
 m1DoseSelector.addEventListener("change", (event) => {
   let m1Dosage = parseFloat(m1DoseSelector.value) * 10;
   client.publish("BioreactorGui/dosingSchedulingM1", m1Dosage.toString(), options);
+  updateConfig();
+
 });
 m2DoseSelector.addEventListener("change", (event) => {
   let m2Dosage = parseFloat(m2DoseSelector.value) * 10;
   client.publish("BioreactorGui/dosingSchedulingM2", m2Dosage.toString(), options);
+  updateConfig();
 });
 m3DoseSelector.addEventListener("change", (event) => {
   let m3Dosage = parseFloat(m3DoseSelector.value) * 10;
   client.publish("BioreactorGui/dosingSchedulingM3", m3Dosage.toString(), options);
+  updateConfig();
 });
 setInterval(updateDosingLabels, 1000);
